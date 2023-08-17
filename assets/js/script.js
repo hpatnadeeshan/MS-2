36673a5dea9f437a6f90be67ffaa92a19d320d12
hpatnadeeshan <>
2023-08-17 16:59:05 +0000

    images added to the directory

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const cards = [
+    { type: 'carrot', image: 'carrot.png', score: 10 },
+    { type: 'trap', image: 'trap.png', score: -5 },
+    { type: 'dog', image: 'dog.png', score: 0 }
+];
+
+const maxLevel = 15;
+const cardsEachLevel = [6, 6, 6, 9, 9, 9, 9, 9, 12, 12, 12, 12, 12, 16, 16];
+
+let score = 0;
+let Level = 1;
+let flippedCards = [];
+
+// Function to shuffle the cards
+
+// Function to assign random images to cards
+
+// Function to flip a card
+
+// Function to check for match 
+
+// Function to start the game
