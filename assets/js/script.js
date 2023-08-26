177073ed0d49470bc199b5bed3c6618ef5535c6a
hpatnadeeshan <>
2023-08-26 18:24:03 +0000

    change image card sizes & fixed a bug at ending of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,12 +106,10 @@ document.addEventListener("DOMContentLoaded", () => {
             cardElement.style.backgroundRepeat = 'no-repeat';
             cardElement.style.backgroundPosition = 'center';
             cardElement.style.backgroundColor = '#8BA799';
-            if (level >= 15) {
-                cardElement.style.backgroundSize = '15% auto';
-            } else if (level >= 12) {
-                cardElement.style.backgroundSize = '20% auto';
+            if (level >= 12) {
+                cardElement.style.backgroundSize = '25% auto';
             } else if (level >= 9) {
-                cardElement.style.backgroundSize = '30% auto';
+                cardElement.style.backgroundSize = '35% auto';
             } else {
                 cardElement.style.backgroundSize = '50% auto';
             }
@@ -290,6 +288,9 @@ document.addEventListener("DOMContentLoaded", () => {
     function restartWithDelay() {
         console.log('restartWithDelay');
         moveToNextLevel();
+        if (gameStatus === 'Won') {
+            return;
+        }
         setTimeout(() => {
             startGame();
             updateLevel();
@@ -364,6 +365,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (gameStatus === 'Won') {
             setTimeout(() => {
                 hideModal();
+                gameStatus = 'progressing';
                 score = 0;
                 level = 1;
                 canClick = false;
